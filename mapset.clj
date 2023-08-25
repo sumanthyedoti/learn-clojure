3e8cff2ea2072ebbdd76919402e1c483de7d5cf6
Sumanth Yeodti <sumanth.yedoti@gmail.com>
2023-08-25 20:07:39 +0530

    Refactor own mapset

--- mapset.clj
+++ mapset.clj
@@ -1,6 +1,7 @@
 ;; a map that returns result in set instead of list
-(defn mapset [f data]
-    (reduce
-     #(conj %1 (f %2))
-     #{}
-     data))
+(defn mapset
+  [f data]
+  (reduce
+    #(conj %1 (f %2))
+    #{}
+    data))
